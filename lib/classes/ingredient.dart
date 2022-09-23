0e5209a17ff2aa23b7e5155175dbc04d5b483b56
Sandor Alin <sandor.alin.nicu@gmail.com>
2022-09-23 16:24:03 +0300

    recipes10 - Adaugare rectiune de ingrediente pe screen-ul de single recipe

---
+++ lib/classes/ingredient.dart
@@ -0,0 +1,13 @@
+class Ingredient {
+  Ingredient({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.um,
+  });
+
+  final int id;
+  final String name;
+  final double quantity;
+  final String um;
+}
