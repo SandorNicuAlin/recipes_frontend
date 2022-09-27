96a5b6ffe941855debbdeae6ebf3e10f6e642d03
Sandor Alin <sandor.alin.nicu@gmail.com>
2022-09-27 12:56:48 +0300

    deploying.. [4]

--- lib/helpers/http_request.dart
+++ lib/helpers/http_request.dart
@@ -1,6 +1,7 @@
 class HttpRequest {
   static const key = '';
-  static const baseUrl = String.fromEnvironment('PRODUCTION') == 'true'
-      ? 'https://lara-recipes.herokuapp.com'
-      : 'http://127.0.0.1:8000';
+  // prod
+  static const baseUrl = 'https://lara-recipes.herokuapp.com';
+  // dev
+  // static const baseUrl = 'http://127.0.0.1:8000';
 }
